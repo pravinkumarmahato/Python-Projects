45ea4ce61bd25c6edd8a1b30e62adba4b759363a
pravinkumarmahato <68019573+pravinkumarmahato@users.noreply.github.com>
2020-11-30 23:29:20 +0530

    Add files via upload

---
+++ Bit_the_Dealer.py
@@ -0,0 +1,10 @@
+print('Pravin Kumar Mahato, Roll No 25')
+import random
+r=random.randint(0,2)
+print(r)
+if r==1:
+    print("Player Wins")
+elif r==2:
+    print("Dealer Wins")
+else:
+    print("Tie")
